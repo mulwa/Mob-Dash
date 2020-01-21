f454e3494e53649f5d0064b0d121813a574f84db
PPTY <mulwatech@gmail.com>
2020-01-21 15:03:38 +0300

    adds user Module UI

--- src/app/pages/users/new-user/new-user.component.ts
+++ src/app/pages/users/new-user/new-user.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { TicketingService } from "../../../services/ticketing.service";
 
 @Component({
-  selector: 'app-new-user',
-  templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.css']
+  selector: "app-new-user",
+  templateUrl: "./new-user.component.html",
+  styleUrls: ["./new-user.component.css"]
 })
 export class NewUserComponent implements OnInit {
+  searchableselect;
 
-  constructor() { }
+  constructor(private ticketingService: TicketingService) {}
 
   ngOnInit() {
+    this.getCity();
   }
 
+  getCity() {
+    this.ticketingService.getCitites().subscribe(
+      data => {
+        // this.cities = data.cities;
+        // console.log(this.searchableselect)
+        this.searchableselect = this.ticketingService.cloneOptions(data.cities);
+        console.log(this.searchableselect);
+      },
+      error => {
+        console.log(`An Error has Occured ${error.message}`);
+      }
+    );
+  }
 }
